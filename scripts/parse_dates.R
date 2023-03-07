79349c2e075f479eef71e382bb79cb19ba120a59
jcblsn <jacobeliason@gmail.com>
2023-03-07 16:22:13 +0000

    upload files

---
+++ scripts/parse_dates.R
@@ -0,0 +1,87 @@
+
+library(tidyverse)
+library(lubridate)
+library(arrow)
+
+# read --------------------------------------------------------------------
+
+
+comments_in <- read_parquet(
+  str_c("files/","100-hour-board-comments",".parquet")
+)
+
+questions_in <- read_parquet(
+  str_c("files/","100-hour-board-questions",".parquet")
+)
+
+# parse -------------------------------------------------------------------
+
+comments <- 
+  comments_in |> 
+  mutate(
+    submission_info = str_remove_all(comment_info, "Question #\\d+ posted on "),
+    submission_info = str_replace_all(submission_info, "midnight", "12:00 a.m."),
+    submission_info = str_replace_all(submission_info, "noon", "12:00 p.m."),
+    submission_info = str_replace_all(submission_info, "a.m.", "am"),
+    submission_info = str_replace_all(submission_info, "p.m.", "pm"),
+    submission_info = str_replace_all(submission_info, "posted on ", ""),
+    submission_info = str_replace(
+      submission_info, 
+      "^\\d{2}/\\d{2}/\\d{4} \\d{1,2} [ap]m$", 
+      str_c(str_sub(submission_info, 1, -4), ":00 ", str_sub(submission_info, -2, -1))
+    ),
+    posted_date = parse_date_time(submission_info, "m/d/Y H:M p", tz = "America/Denver"), 
+    posted_date_utc = as_datetime(posted_date, tz = "utc"),
+    posted_date = format(posted_date, "%Y-%m-%d %H:%M:%S %Z"),
+    posted_date_utc = format(posted_date_utc, "%Y-%m-%d %H:%M:%S %Z")
+  ) |> 
+  select(-submission_info, -comment_info)
+
+questions <- 
+  questions_in |> 
+  mutate(
+    submission_info = str_remove_all(question_info, "Question #\\d+ posted on "),
+    submission_info = str_replace_all(submission_info, "midnight", "12:00 a.m."),
+    submission_info = str_replace_all(submission_info, "noon", "12:00 p.m."),
+    submission_info = str_replace_all(submission_info, "a.m.", "am"),
+    submission_info = str_replace_all(submission_info, "p.m.", "pm"),
+    submission_info = str_replace_all(submission_info, "posted on ", ""),
+    submission_info = str_replace(
+      submission_info, 
+      "^\\d{2}/\\d{2}/\\d{4} \\d{1,2} [ap]m$", 
+      str_c(str_sub(submission_info, 1, -4), ":00 ", str_sub(submission_info, -2, -1))
+    ),
+    posted_date = parse_date_time(submission_info, "m/d/Y H:M p", tz = "America/Denver"), 
+    posted_date_utc = as_datetime(posted_date, tz = "utc"),
+    posted_date = format(posted_date, "%Y-%m-%d %H:%M:%S %Z"),
+    posted_date_utc = format(posted_date_utc, "%Y-%m-%d %H:%M:%S %Z")
+  ) |> 
+  select(-submission_info, -question_info)
+
+# questions |> 
+#   group_by(year = year(posted_date), month = month(posted_date)) |> 
+#   count() |> 
+#   ungroup() |> 
+#   mutate(dt = as_datetime(str_c(year,"-",month,"-01"))) |> 
+#   ggplot() +
+#   geom_line(
+#     aes(x = dt, y = n)
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     axis.title = element_blank()
+#   )
+  
+# comments |>
+#   group_by(year = year(posted_date), month = month(posted_date)) |>
+#   count() |>
+#   ungroup() |>
+#   mutate(dt = as_datetime(str_c(year,"-",month,"-01"))) |>
+#   ggplot() +
+#   geom_line(
+#     aes(x = dt, y = n)
+#   ) +
+#   theme_minimal() +
+#   theme(
+#     axis.title = element_blank()
+#   )
